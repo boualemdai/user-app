94d43b64717b771e59a11f0c35c5e8fa0dd2ab97
boualem <boualemdai@gmail.com>
2022-01-13 18:24:48 +0100

    premiere version

---
+++ index.js
@@ -0,0 +1,28 @@
+//https://randomuser.me/api/?results=24
+
+let userData = [];
+
+const fetchUser = async () => {
+  await fetch("https://randomuser.me/api/?results=24")
+    .then((res) => res.json())
+    .then((data) => (userData = data.results));
+  console.log(userData);
+};
+const userDisplay = async () => {
+  await fetchUser();
+
+  document.body.innerHTML = userData
+    .map(
+      (user) =>
+        `
+      <div class='card'>
+      <img src=${user.picture.large} alt = 'photo de ${user.name.last}'>
+      <h3>${user.name.first}</h3>
+      <p>${user.location.city},${user.dob.date}</p>
+      <em>memebre depuis: ${user.registered.date} jours </em>
+      </div>
+      `
+    )
+    .join("");
+};
+userDisplay();
